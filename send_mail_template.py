7d441af2df593ea1fb2516fb2dea7771d2a7522d
Farrah <farrah@cloudartadmins.com>
2024-10-07 20:28:46 -0700

    added warning about possible incorrect option use

--- send_mail_template.py
+++ send_mail_template.py
@@ -10,10 +10,12 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 parser = argparse.ArgumentParser(prog="send_mail_template.py",
     description="Sends an email based on template.")
 parser.add_argument('filename')
+parser.add_argument('partner_type')
 args = parser.parse_args()
 print(args.filename)
+print(args.partner_type)
 
-def send_template_email(template, to_email, subj, **kwargs):
+def send_template_email(template, to_email, subj, cc, **kwargs):
     env = Environment(
         loader = FileSystemLoader('email_templates'),
         autoescape = select_autoescape(['html', 'xml'])
@@ -21,13 +23,14 @@ def send_template_email(template, to_email, subj, **kwargs):
 
     template=env.get_template(template)
 
-    send_email(to_email, subj, template.render(**kwargs))
+    send_email(to_email, subj, cc, template.render(**kwargs))
 
-def send_email(to_email, subj, body):
+def send_email(to_email, subj, cc, body):
     html_message = MIMEText(body, 'html')
     html_message['Subject'] = subj
     html_message['From'] = SENDER_EMAIL
     html_message['To'] = to_email
+    html_message['Cc'] = cc
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
         server.login(SENDER_EMAIL, SENDER_PASSWORD)
@@ -40,7 +43,16 @@ with open(args.filename) as csvfile:
 
     for row in csvreader:
         org_name=row["Organization"]
-        subj=org_name + " as a SeaGL 2024 Sponsor?"
+        if (args.partner_type == "sponsor"):
+            subj=org_name + " as a SeaGL 2024 Sponsor?"
+        elif (args.partner_type == "partner"):
+            subj=org_name + " as a SeaGL 2024 Partner?"
+        else:
+            print ("Warning: partner type incorrectly input or inexistent.")
+            print ("Current checks are for..")
+            print (" - sponsor")
+            print (" - partner")
+            
 
         # cascadia? returning?  result
         # no        no          regular
@@ -57,11 +69,14 @@ with open(args.filename) as csvfile:
             template = "./SeaGL_2024_local_partner_Template.html"
         else:
             template = "./SeaGL_2024_Sponsor_Template.html"
+
+        cc_email="partnerships@seagl.org"
             
         send_template_email(
             template=template,
             to_email=row["Contact_Email"],
             subj=subj,
+            cc=cc_email,
             CONTACT_FIRST_NAME=row["Contact_Name"],
             SENDER_FIRST_NAME=SENDER_FIRST_NAME,
             ORG_NAME=org_name
